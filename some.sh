5d0e5fa3a9b572e2ccac7219a5ebb65d60a248a0
digitalya-eusebiugagea <105045525+digitalya-eusebiugagea@users.noreply.github.com>
2023-03-07 12:34:57 +0200

    Update some.sh

--- some.sh
+++ some.sh
@@ -1,10 +1,8 @@
 GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 echo $GIT_COMMIT_MSG
 SUB='chore(release)'
-if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]
-then
-  echo "The commit is a release one. The pipeline shouldn't be triggered."
+if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]; then
+  echo "It's there."
   exit 1
-else
-  echo "The commit is not a release one"
 fi
+echo "All good"
