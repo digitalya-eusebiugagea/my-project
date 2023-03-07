d64cb9c368154dbd03b25bed62b93204fac3992f
Gagea Eusebiu Andrei <gageaeusebiuandrei@Gageas-MacBook-Pro.local>
2023-03-07 15:26:33 +0200

    refactor: cleanup

--- some.sh
+++
@@ -1,8 +0,0 @@
-GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
-echo $GIT_COMMIT_MSG
-SUB='chore(release)'
-if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]; then
-  echo "It's there."
-  exit 1
-fi
-echo "All good"
