e4a3ab17c76aa1067065266892ce73c7ac095092
digitalya-eusebiugagea <105045525+digitalya-eusebiugagea@users.noreply.github.com>
2023-03-07 12:23:44 +0200

    Update some.sh

--- some.sh
+++ some.sh
@@ -1,6 +1,5 @@
 GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 SUB='chore(release)'
-
 if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]
 then
   echo "The commit is a release one so the pipeline shouldn't be trigerred"
