0005cd82fc50afaef7afdfb6cb2ae2189aa0b1e1
digitalya-eusebiugagea <105045525+digitalya-eusebiugagea@users.noreply.github.com>
2023-03-07 11:01:47 +0200

    Update some.sh

--- some.sh
+++ some.sh
@@ -1,5 +1,5 @@
-STR='GNU/Linux is an operating system'
-SUB='Linux'
-if [[ "$STR" == *"$SUB"* ]]; then
+GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
+SUB='chore(release)'
+if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]; then
   echo "It's there."
 fi
