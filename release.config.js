821990a99f5a9473a3c8a37b6d676448645865a5
Gagea Eusebiu Andrei <gageaeusebiuandrei@Gageas-MacBook-Pro.local>
2023-03-07 16:05:23 +0200

    docs: abc

--- release.config.js
+++ release.config.js
@@ -1,6 +1,21 @@
 module.exports = {
     "plugins": [
-        "@semantic-release/commit-analyzer",
+        ["@semantic-release/commit-analyzer", {
+            "preset": "conventionalcommits",
+            "releaseRules": [
+                {"type": "revert", "section":"Reverts", "hidden": false},
+                {"type": "feat", "section": "Features", "hidden": false},
+                {"type": "fix", "section": "Bug Fixes", "hidden": false},
+                {"type": "improvement", "section": "Feature Improvements", "hidden": false},
+                {"type": "docs", "section":"Docs", "hidden": false},
+                {"type": "style", "section":"Styling", "hidden": false},
+                {"type": "refactor", "section":"Code Refactoring", "hidden": false},
+                {"type": "perf", "section":"Performance Improvements", "hidden": false},
+                {"type": "test", "section":"Tests", "hidden": false},
+                {"type": "build", "section":"Build System", "hidden": false},
+                {"type": "ci", "section":"CI", "hidden":false}
+            ]
+        }],
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
         "@semantic-release/github",
