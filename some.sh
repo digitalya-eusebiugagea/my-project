2616dfbb119ed7702834b1c27b617371a8b67737
digitalya-eusebiugagea <105045525+digitalya-eusebiugagea@users.noreply.github.com>
2023-03-07 11:16:49 +0200

    Update some.sh

--- some.sh
+++ some.sh
@@ -1,5 +1,8 @@
 GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 SUB='chore(release)'
-if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]; then
+if [[ "$GIT_COMMIT_MSG" == *"$SUB"* ]]
+then
   $NON_RELEASE=true
+else
+  echo $NON_RELEASE=false
 fi
